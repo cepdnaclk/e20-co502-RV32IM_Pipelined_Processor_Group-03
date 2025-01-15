54814ed7d2948f4f1cea5db6626b744e13a759f7
Devin-Hasnaka <e20385@eng.pdn.ac.lk>
2025-01-15 14:55:56 +0530

    CPU created

---
+++ verilog/CPU/CPU.v
@@ -0,0 +1,6 @@
+`timescale 1ns / 1ps    
+
+`include "../instruction decode stage/instruction_decode.v"
+`include "../instruction fetch stage/instruction_fetch.v"
+`include "../Memory Access stage/memory_access.v"
+`include "../instruction execution stage/instruction_execution.v"
