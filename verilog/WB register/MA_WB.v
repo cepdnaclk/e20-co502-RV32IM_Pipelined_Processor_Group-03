d4d666a86ccc70220433d2b77ab04b793f789f00
Samuditha Seneviratne <150885543+gssamuditha@users.noreply.github.com>
2025-01-25 22:56:32 +0530

    Update MA_WB.v

--- verilog/WB register/MA_WB.v
+++ verilog/WB register/MA_WB.v
@@ -6,20 +6,20 @@
 
 `timescale  1ns/100ps
 
+`timescale 1ns/100ps
+
 module EX_MA_register(
     input CLK,
     input MUX3_select,
     input regwrite_enable,
-    input ALU_out [31:0],
-    input read_data [31:0],
-    input rd [4:0]
-
+    input [31:0] ALU_out,
+    input [31:0] read_data,
+    input [4:0] rd,
     output reg MUX3_select_out,
     output reg regwrite_enable_out,
     output reg [31:0] ALU_out_out,
     output reg [31:0] read_data_out,
-    output reg [4:0] rd_out 
-
+    output reg [4:0] rd_out
 );
 
     always @(posedge CLK) begin
